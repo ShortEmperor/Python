7ae9d575c8cfb61b40e74fca00badf8a0e7dde7e
Fabricio <ShortEmperor194@gmail.com>
2020-08-27 03:34:50 -0500

    Reversing to some old code, need a fix ASAP

--- xmasTry.py
+++ xmasTry.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3.8
-import datetime
-
-x = datetime.datetime(2020, 12, 24)
-y = datetime.datetime.now()
-print(x-y)
+from datetime import *
+date_format = "%m/%d/"
+now = datetime.now()
+xmas = datetime(now.year, 12, 25)
+delta = xmas - now
+final= delta.days
+if final > 0:
+ print(final, "days until Christmas!")
+elif final == 0:
+ print("Merry Christmas!")
+elif final < 0:
+ print("It is past Christmas, wait until next year!")
