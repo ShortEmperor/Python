b922ccd25cdeb72f3ad4c2bed021a73db4449dc5
Fabricio <ShortEmperor194@gmail.com>
2020-08-27 03:31:46 -0500

    Reversing to hello world

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3.8
 
-import sys
-
-if len(sys.argv) == 2:
-    print("Knock, Knock, {0}".format(sys.argv[1]))
-else:
-    sys.stderr.write("Usage: {0} <name>".format(sys.argv[0]))
-    
+print('Hello world')
